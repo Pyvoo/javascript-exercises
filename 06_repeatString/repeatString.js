79d75ea13072144fa5760d0a44b1d3e3e8b0da58
Andy Truong <pyvooo@gmail.com>
2025-11-27 16:38:04 -0600

    complete exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+    if (count < 0) return "ERROR";
+    
+    let ans = "";
+    for (let i = 0; i < count; i++)
+        ans += str;
+    return ans;
 };
 
 // Do not edit below this line
